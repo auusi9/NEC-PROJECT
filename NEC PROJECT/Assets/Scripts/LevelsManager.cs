1671222a906a3eb9914f22cf6769df9b7fe131cf
kellazo <gilli11519@gmail.com>
2016-02-15 19:32:55 +0100

    UI Menu
    Under construction

---
+++ NEC PROJECT/Assets/Scripts/LevelsManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class LevelsManager : MonoBehaviour {
+
+    public Button lvl1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        lvl1 = lvl1.GetComponent<Button>();
+
+    }
+
+    public void LevelChose()
+    {
+        SceneManager.LoadScene("NECSCENE");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ NEC PROJECT/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+    public Button Bplay;
+
+	// Use this for initialization
+	void Start () {
+
+        Bplay = Bplay.GetComponent<Button>();
+	
+	}
+
+    public void LevelChose()
+    {
+        SceneManager.LoadScene("UILevels");
+    }
+        
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
