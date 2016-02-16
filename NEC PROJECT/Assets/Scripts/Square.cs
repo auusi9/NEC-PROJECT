0e9e0e9ccbb31d1c69f6be96854feb134442a0c2
auusi9 <ausi_dalmau@hotmail.com>
2016-02-16 19:33:10 +0100

    sprite square

--- NEC PROJECT/Assets/Scripts/Square.cs
+++ NEC PROJECT/Assets/Scripts/Square.cs
@@ -1,27 +1,57 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Square : MonoBehaviour {
 
     public Rigidbody2D square;
     float angle = 1.0f;
-	// Use this for initialization
-	void Start () 
+    float prev_rotation;
+    private Vector2 centerPoint;
+
+    // Use this for initialization
+    void Start () 
     {
-	    
-	}
+        Input.simulateMouseWithTouches = true;
+    }
 	
 	// Update is called once per frame
 	void Update () 
     {
         float rotation = square.rotation;
-        if(Input.GetButton("Fire1"))
+
+        /*
+
+        Vector2 fingermovement =  Input.GetTouch(0).deltaPosition;
+
+        if (fingermovement.x < 0)
         {
             square.MoveRotation(rotation + angle);
+
         }
-        else if(Input.GetButton("Fire2"))
+        else if (fingermovement.x > 0)
         {
+
             square.MoveRotation(rotation - angle);
-        }
-	}
+
+        }*/
+        
+
+        
+                if(Input.GetButton("Fire1"))
+                {
+                    square.MoveRotation(rotation + angle);
+                }
+                else if(Input.GetButton("Fire2"))
+                {
+                    square.MoveRotation(rotation - angle);
+                }
+    }
+
+
+
+
+
+
 }
+
