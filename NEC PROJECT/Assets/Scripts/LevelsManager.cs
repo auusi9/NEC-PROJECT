0e18c2359bc5e543b076cdebadd62178bb7b4ec7
iorek <Administrador@TR12-202-51.tr12.cat>
2016-02-23 12:03:28 +0100

    UI In-game

--- NEC PROJECT/Assets/Scripts/LevelsManager.cs
+++ NEC PROJECT/Assets/Scripts/LevelsManager.cs
@@ -5,25 +5,35 @@ using System.Collections;
 
 public class LevelsManager : MonoBehaviour {
 
-    public Button lvl1;
-
     // Use this for initialization
     void Start()
     {
 
-        lvl1 = lvl1.GetComponent<Button>();
-
     }
 
-    public void LevelChose()
+    public void LoadScene1()
     {
         SceneManager.LoadScene("NECSCENE");
     }
 
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("NECSCENE2");
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("NECSCENE3");
+    }
+
+    public void LoadScene4()
+    {
+        SceneManager.LoadScene("NECSCENE4");
+    }
 
     // Update is called once per frame
     void Update()
     {
-
+    
     }
 }
---
+++ NEC PROJECT/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public string levelSelect;
+
+    public string mainMenu;
+
+    public GameObject Pause_Menu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0f;
+        Pause_Menu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        Pause_Menu.SetActive(false);
+    }
+
+    public void LevelSelect()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(levelSelect);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenu);
+    }
+}
