f4b92f97ede2313ae6fe224d7ef219c18cacc373
auusi9 <ausi_dalmau@hotmail.com>
2016-02-27 21:29:23 +0100

    PlayScene Before Level Starting

---
+++ NEC PROJECT/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+
+
+    public string levelSelect;
+
+    public GameObject PauseSystem;
+
+    string mainMenu = "Menu";
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f;
+        this.gameObject.SetActive(false);
+        PauseSystem.SetActive(true);
+    }
+
+    public void LevelSelect()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(levelSelect);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(mainMenu);
+    }
+}
