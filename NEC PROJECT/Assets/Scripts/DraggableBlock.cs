698387157a6b23c45f928762729c5f4ceae8bd8f
auusi9 <ausi_dalmau@hotmail.com>
2016-03-02 01:04:49 +0100

    Touch Input MultitouchStarted

--- NEC PROJECT/Assets/Scripts/DraggableBlock.cs
+++ NEC PROJECT/Assets/Scripts/DraggableBlock.cs
@@ -7,13 +7,14 @@ public class DraggableBlock : MonoBehaviour {
     private Vector3 offset;
     public Rigidbody2D Draggableblock;
     Vector3 curPosition;
+    /*
     void OnMouseDown()
     {
+
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
 
 
         offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
     }
 
 
@@ -33,5 +34,44 @@ public class DraggableBlock : MonoBehaviour {
 
         Draggableblock.velocity = Vector3.zero;
     }
- 
+    */
+    
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+
+
+
+
+                Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                {
+                    screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+                    offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, screenPoint.z));
+                }
+                
+
+                
+                
+            }
+            else if (Input.GetTouch(i).phase == TouchPhase.Moved)
+            {
+                Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchPos))
+                {
+
+                    Vector3 curScreenPoint = new Vector3(Input.GetTouch(i).position.x, Input.GetTouch(i).position.y, screenPoint.z);
+                    curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+                    Draggableblock.velocity = curPosition * 3;
+                }
+                
+            }
+        }
+    }
+
 }
--- NEC PROJECT/Assets/Scripts/SpinScript.cs
+++ NEC PROJECT/Assets/Scripts/SpinScript.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class SpinScript : MonoBehaviour
 {
 
-    private float baseAngle = 0.0f;
+    private float[] baseAngle = new float[5];
+    Vector2[] posBegan = new Vector2[5];
+    Vector2[] posMoved = new Vector2[5];
     Quaternion iniRot;
-    void OnMouseDown()
+
+    /*void OnMouseDown()
     {
         Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
         pos = Input.mousePosition - pos;
@@ -20,13 +23,49 @@ public class SpinScript : MonoBehaviour
         pos = Input.mousePosition - pos;
         float ang = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg - baseAngle;
         transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
-    }
+    }*/
 
     void Start()
     {
         iniRot = transform.rotation;
 
+        
+
     }
+
+
+    void Update()
+    {
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                 Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                if (GetComponent<Collider2D>().OverlapPoint(touchPos))
+                {
+                    posBegan[i] = Camera.main.WorldToScreenPoint(transform.position);
+                    posBegan[i] = Input.GetTouch(i).position - posBegan[i];
+                    baseAngle[i] = Mathf.Atan2(posBegan[i].y, posBegan[i].x) * Mathf.Rad2Deg;
+                    baseAngle[i] -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+                }
+                
+            }
+            else if(Input.GetTouch(i).phase == TouchPhase.Moved)
+            {
+                Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                if (GetComponent<Collider2D>().OverlapPoint(touchPos))
+                {
+                    posMoved[i] = Camera.main.WorldToScreenPoint(transform.position);
+                    posMoved[i] = Input.GetTouch(i).position - posMoved[i];
+                    float ang = Mathf.Atan2(posMoved[i].y, posMoved[i].x) * Mathf.Rad2Deg - baseAngle[i];
+                    transform.rotation = Quaternion.AngleAxis(ang, Vector3.forward);
+                }
+            }
+        }
+    }
+
     void LateUpdate()
     {
         if(Time.timeScale == 0 )
